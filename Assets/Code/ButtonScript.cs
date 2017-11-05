ce817f60cb500b6a02fac55710337a48bafa6854
Andy Han <dhan@macalester.edu>
2017-11-05 11:12:45 -0600

    Added Main Menu with Scenes

---
+++ Assets/Code/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// SpaceWanderer 
+// November 5th, 2017
+// Team Anhagama 
+
+public class ButtonScript : MonoBehaviour {
+
+	public void LoadScene()
+	{
+		Application.LoadLevel("GameScene");
+	}
+
+	public void LoadAbout()
+	{
+		Application.LoadLevel("About");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
