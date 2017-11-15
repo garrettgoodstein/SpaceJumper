2ec0dd2a6860da51502773d5e82bdccf27a3fa09
Andy Han <dhan@macalester.edu>
2017-11-14 20:51:16 -0600

    things move

---
+++ Assets/Code/PlanetTouchBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetTouchBehavior : MonoBehaviour {
+	public float speed = 0.1F;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			// The screen has been touched so store the touch
+			Touch touch = Input.GetTouch (0);
+
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+				// If the finger is on the screen, move the object smoothly to the touch position
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y));
+				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime); 
+			}
+		}
+}
+}
