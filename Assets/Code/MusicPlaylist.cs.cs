3df936cc854db2ab05b34ff5ab4a6aae1275074a
Andy Han <dhan@macalester.edu>
2017-11-16 19:39:50 -0600

    changed name of musicplaylist

--- Assets/Code/MusicPlaylist.cs.cs
+++ Assets/Code/MusicPlaylist.cs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicPlaylist.cs : MonoBehaviour {
+public class MusicPlaylist : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- Assets/Code/PlanetTouchBehavior.cs
+++ Assets/Code/PlanetTouchBehavior.cs
@@ -15,7 +15,7 @@ public class PlanetTouchBehavior : MonoBehaviour {
 			// The screen has been touched so store the touch
 			Touch touch = Input.GetTouch (0);
 
-			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+			if (touch.phase == TouchPhase.Moved) {
 				// If the finger is on the screen, move the object smoothly to the touch position
 				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y));
 				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime); 
