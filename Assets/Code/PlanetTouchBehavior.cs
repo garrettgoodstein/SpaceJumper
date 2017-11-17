2069a64a2fb606ed835a061a24edc47f586adc0e
Andy Han <dhan@macalester.edu>
2017-11-16 20:16:47 -0600

    some changes..

--- Assets/Code/PlanetTouchBehavior.cs
+++ Assets/Code/PlanetTouchBehavior.cs
@@ -14,8 +14,9 @@ public class PlanetTouchBehavior : MonoBehaviour {
 		if (Input.touchCount > 0) {
 			// The screen has been touched so store the touch
 			Touch touch = Input.GetTouch (0);
+			var clickedPosition = touch.position;
 
-			if (touch.phase == TouchPhase.Moved) {
+			if ((touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved ) || Physics.CheckSphere(clickedPosition, 100)) {
 				// If the finger is on the screen, move the object smoothly to the touch position
 				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y));
 				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime); 
