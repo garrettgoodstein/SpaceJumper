b0a76f0957fac4c5e972e0ed18da842856c1b9cf
Andy Han <dhan@macalester.edu>
2017-11-19 13:18:51 -0600

    worked on some stuff with the touch

--- Assets/Code/PlanetTouchBehavior.cs
+++ Assets/Code/PlanetTouchBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetTouchBehavior : MonoBehaviour {
-	public float speed = 0.1F;
+	public float speed = 0.0F;
 	public Vector3 constantScale = new Vector3 (62, 30);
 
 	// Use this for initialization
@@ -19,15 +19,27 @@ public class PlanetTouchBehavior : MonoBehaviour {
 			Vector3 touchPos = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y));
 			Vector2 mousePos2D = new Vector3(touchPos.x,touchPos.y);
 
-			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-			if (hit.collider != null) {
-				if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved ) {
-				// If the finger is on the screen, move the object smoothly to the touch position
+			var princePositionX = GameObject.FindGameObjectWithTag("Prince").transform.position.x;
+			var princePositionY = GameObject.FindGameObjectWithTag ("Prince").transform.position.y;
+			var princePositionZ = GameObject.FindGameObjectWithTag ("Prince").transform.position.z;
+
+
+			var touchPositionX = touchPos.x;
+			var touchPositionY = touchPos.y;
+			var touchPositionZ = touchPos.z;
+
+			var touchDifference = touchPositionZ - princePositionZ;
+			print (touchPositionY);
+//			print (princePosition);
+
+			if ((touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved ) && touchDifference < -800) {
+			// If the finger is on the screen, move the object smoothly to the touch position
+				transform.localScale += new Vector3(0.0F, 0, 10);
 				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y));
 				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime); 
-
-			}
+			
 		}
+		}
+	}
 }
-}
-}
+
