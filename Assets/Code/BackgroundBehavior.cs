f193c6f4f711419108f48a99b9913fd94d93fbb7
Andy Han <dhan@macalester.edu>
2017-11-22 23:49:10 -0600

    worked on moving background stuff.

---
+++ Assets/Code/BackgroundBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour {
+
+	static float THRESHOLD = 1;
+
+	float initX;
+	float initY;
+	float initZ;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = transform.GetComponent<Rigidbody>();
+		initX = transform.position.x;
+		initY = transform.position.y;
+		initZ = transform.position.z;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int randTypeForce = Random.Range (0, 3);
+		int force = Random.Range (0, 3); 
+
+
+		if (randTypeForce == 0) {
+			rb.AddForce (0, force, force);
+		}
+		if (randTypeForce == 1) {
+			rb.AddForce (force, 0, 0);
+		}
+		if (randTypeForce == 2) {
+			rb.AddForce (0, -force, -force);
+		}
+		if (randTypeForce == 3) {
+			rb.AddForce (-force, 0, 0);
+		}
+
+
+		if (transform.position.x > initX + THRESHOLD) {
+			rb.AddForce (-force, 0, 0);
+			//Debug.Log ("if: "+transform.position.x);
+		} else {
+			rb.AddForce (force, 0, 0);
+			//Debug.Log ("else: "+transform.position.x);
+		}
+
+		if (transform.position.y > initY + THRESHOLD) {
+			rb.AddForce (0, -force, 0);
+			//Debug.Log ("if: "+transform.position.y);
+		} else {
+			rb.AddForce (0, force, 0);
+			//Debug.Log ("else: "+transform.position.y);
+		}
+
+//		if (transform.position.z > initZ + THRESHOLD*1000) {
+//			rb.AddForce (0, 0, -force);
+//			//Debug.Log ("if: "+transform.position.y);
+//		} else {
+//			rb.AddForce (0, 0, force);
+//			//Debug.Log ("else: "+transform.position.y);
+//		}
+	}
+}
