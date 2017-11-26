bc5c7c901dd35cb6981c78e57619c37501953f8d
Andy Han <dhan@macalester.edu>
2017-11-26 14:06:20 -0600

    worked on the general touch things

--- Assets/Code/PlanetTouchBehavior.cs
+++ Assets/Code/PlanetTouchBehavior.cs
@@ -18,10 +18,7 @@ public class PlanetTouchBehavior : MonoBehaviour {
 			if ((touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)) {
 				// If the finger is on the screen, move the object smoothly to the touch position
 				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 0));
-				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime); 
-
-				transform.position += new Vector3(0,0,100);
-				transform.position += new Vector3(0,0,-100);
+				transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime);
 
 			
 			}
