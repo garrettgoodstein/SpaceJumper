22666f48b21f16ab5ddccf998f332a2054c28b34
Andy Han <dhan@macalester.edu>
2017-11-26 13:22:52 -0600

    added stability to moving background code...

--- Assets/Code/BackgroundBehavior.cs
+++ Assets/Code/BackgroundBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackgroundBehavior : MonoBehaviour {
 
-	static float THRESHOLD = 3;
+	static float THRESHOLD = 80;
 
 	float initX;
 	float initY;
@@ -24,37 +24,41 @@ public class BackgroundBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		int randTypeForce = Random.Range (0, 3);
-		int force = Random.Range (0, 3); 
+		int force = Random.Range (70,100); 
 
 
 		if (randTypeForce == 0) {
-			rb.AddForce (0, force, force);
+			rb.AddForce (0, force, 0);
+			print ("1 Is this shit even working?");
 		}
 		if (randTypeForce == 1) {
 			rb.AddForce (force, 0, 0);
+			print ("2 Is this shit even working?");
 		}
 		if (randTypeForce == 2) {
-			rb.AddForce (0, -force, -force);
+			rb.AddForce (0, -force, 0);
+			print ("3 Is this shit even working?");
 		}
 		if (randTypeForce == 3) {
 			rb.AddForce (-force, 0, 0);
+			print ("4 Is this shit even working?");
 		}
 
 
 		if (transform.position.x > initX + THRESHOLD) {
 			rb.AddForce (-force, 0, 0);
-			//Debug.Log ("if: "+transform.position.x);
+//			Debug.Log ("if: "+transform.position.x);
 		} else {
 			rb.AddForce (force, 0, 0);
-			//Debug.Log ("else: "+transform.position.x);
+//			Debug.Log ("else: "+transform.position.x);
 		}
 
 		if (transform.position.y > initY + THRESHOLD) {
 			rb.AddForce (0, -force, 0);
-			//Debug.Log ("if: "+transform.position.y);
+//			Debug.Log ("if: "+transform.position.y);
 		} else {
 			rb.AddForce (0, force, 0);
-			//Debug.Log ("else: "+transform.position.y);
+//			Debug.Log ("else: "+transform.position.y);
 		}
 	}
 }
