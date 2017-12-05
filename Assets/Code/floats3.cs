0cc3e1062324e0d6c74188ad8c24636cb34dd02b
Andy Han <dhan@macalester.edu>
2017-12-05 17:00:25 -0600

    fixed compiler error

--- Assets/Code/floats3.cs
+++ Assets/Code/floats3.cs
@@ -1,56 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class floats3 : MonoBehaviour {
-
-	static float THRESHOLD = 30;
-
-	float initX;
-	float initY;
-	float initZ;
-	Rigidbody rb;
-
-	// Use this for initialization
-	void Start () {
-
-		rb = transform.GetComponent<Rigidbody>();
-		initX = transform.position.x;
-		initY = transform.position.y;
-		initZ = transform.position.z;
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		int randTypeForce = Random.Range (0, 3);
-		int force = Random.Range (2,5); 
-
-
-		if (randTypeForce == 0) {
-			rb.AddForce (0, force, 0);
-		}
-		if (randTypeForce == 1) {
-			rb.AddForce (force, 0, 0);
-		}
-		if (randTypeForce == 2) {
-			rb.AddForce (0, -force, 0);
-		}
-		if (randTypeForce == 3) {
-			rb.AddForce (-force, 0, 0);
-		}
-
-
-		if (transform.position.x > initX + THRESHOLD) {
-			rb.AddForce (-force, 0, 0);
-		} else {
-			rb.AddForce (force, 0, 0);
-		}
-
-		if (transform.position.y > initY + THRESHOLD) {
-			rb.AddForce (0, -force, 0);
-		} else {
-			rb.AddForce (0, force, 0);
-		}
-	}
-}
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class floats3 : MonoBehaviour {
+//
+//	static float THRESHOLD = 30;
+//
+//	float initX;
+//	float initY;
+//	float initZ;
+//	Rigidbody rb;
+//
+//	// Use this for initialization
+//	void Start () {
+//
+//		rb = transform.GetComponent<Rigidbody>();
+//		initX = transform.position.x;
+//		initY = transform.position.y;
+//		initZ = transform.position.z;
+//
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//		int randTypeForce = Random.Range (0, 3);
+//		int force = Random.Range (2,5); 
+//
+//
+//		if (randTypeForce == 0) {
+//			rb.AddForce (0, force, 0);
+//		}
+//		if (randTypeForce == 1) {
+//			rb.AddForce (force, 0, 0);
+//		}
+//		if (randTypeForce == 2) {
+//			rb.AddForce (0, -force, 0);
+//		}
+//		if (randTypeForce == 3) {
+//			rb.AddForce (-force, 0, 0);
+//		}
+//
+//
+//		if (transform.position.x > initX + THRESHOLD) {
+//			rb.AddForce (-force, 0, 0);
+//		} else {
+//			rb.AddForce (force, 0, 0);
+//		}
+//
+//		if (transform.position.y > initY + THRESHOLD) {
+//			rb.AddForce (0, -force, 0);
+//		} else {
+//			rb.AddForce (0, force, 0);
+//		}
+//	}
+//}
