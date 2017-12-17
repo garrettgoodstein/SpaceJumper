ad413509e213bf3ff27fd85ed358a749586afd1c
Andy Han <dhan@macalester.edu>
2017-12-17 16:22:57 -0600

    refactored and deleted a file

--- Assets/Code/BackgroundBehavior.cs
+++ Assets/Code/BackgroundBehavior.cs
@@ -3,43 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundBehavior : MonoBehaviour {
+	// This class is responsible for allowing background objects to move in various directions 
+	// to give a fluid experience. 
 
-	static float THRESHOLD = 80;
+	static float THRESHOLD = 80; // This threshold is needed to ensure that the background does not go too far in one direction. 
 
+	// axis variables for the background
 	float initX;
 	float initY;
 	float initZ;
-	Rigidbody rb;
+	Rigidbody rb; // the Rigidbody object feature in Unity is what moves the object. Needs a variable to use it. 
 
 	// Use this for initialization
 	void Start () {
-
 		rb = transform.GetComponent<Rigidbody>();
 		initX = transform.position.x;
 		initY = transform.position.y;
 		initZ = transform.position.z;
-
 	}
 
 	// Update is called once per frame
 	void Update () {
-		int randTypeForce = Random.Range (0, 3);
-		int force = Random.Range (30,50); 
-
+		int randTypeForce = Random.Range (0, 3); // Picks a random number between 0 and 3 to determine the direction to go to. 
+		int force = Random.Range (30,50); // Picks a random force between 30 and 50. 
 
 		if (randTypeForce == 0) {
-			rb.AddForce (0, force, 0);
+			rb.AddForce (0, force, 0); // move up 
 		}
 		if (randTypeForce == 1) {
-			rb.AddForce (force, 0, 0);
+			rb.AddForce (force, 0, 0); // move right 
 		}
 		if (randTypeForce == 2) {
-			rb.AddForce (0, -force, 0);
+			rb.AddForce (0, -force, 0); // move down 
 		}
 		if (randTypeForce == 3) {
-			rb.AddForce (-force, 0, 0);
+			rb.AddForce (-force, 0, 0); // move left 
 		}
 
+		// The two methods below ensure that the background does not go beyond the threshold. 
 
 		if (transform.position.x > initX + THRESHOLD) {
 			rb.AddForce (-force, 0, 0);
